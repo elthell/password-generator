e4588b97034d813bc58a3c0976e5d4e740f0f9e7
elthell <eleanor.thell@gmail.com>
2023-06-29 11:30:36 -0500

    simply variable strings + update readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Portfolio
+
+## Description
+The password generator allows users to create secure passwords based on criteria they've selected, including how long the password is and what kind of characters it includes.
+
+## Installation
+N/A
+
+## Usage
+Deployment: 
+
+Preview:
+![A white page with a textbox in the center and a red button saying "Generate Passoword underneath.]()
+
+## Credits
+
+Addition assignment: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment
+
+Specifically adding characters to a chosen empty string variable: https://stackoverflow.com/questions/59904757/random-password-generator-javascript-not-returning-password
+
+## License
+N/A
--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // All possible inclusions
-const lowerCaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z']; 
-const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+const numericCharacters = "0123456789";
+const specialCharacters = "!#$%&()*+-./:;<=>?@[\^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
@@ -54,7 +54,6 @@ function generatePassword() {
 
   // Randomize inclusions and add 
   for (let i = 0; i < passwordLength; i++) {
-    console.log(passwordInclusions[Math.floor(Math.random() * passwordInclusions.length)]);
     let randomInclusions = passwordInclusions[Math.floor(Math.random() * passwordInclusions.length)]
     // Fill new password with random inclusions
     newPassword += randomInclusions;
@@ -62,9 +61,3 @@ function generatePassword() {
 
   return newPassword 
 }
-
-// Random selection of inclusions
-//const randomLowerCase = Math.floor(Math.random() * lowerCaseLetters.length)
-//const randomUpperCase = Math.floor(Math.random() * upperCaseLetters.length);
-//const randomNumbers = Math.floor(Math.random() * numericCharacters.length);
-//const randomSpecial = Math.floor(Math.random() * specialCharacters.length);
